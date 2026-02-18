0859c73c6460e9f87f461db896d826f105284d6f
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 22:35:51 -0500

    ProductoVM Ok

--- CarritoDeCompras/CarritoDeCompras/Models/ProductoVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/ProductoVM.cs
@@ -1,6 +1,21 @@
-﻿namespace CarritoDeCompras.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarritoDeCompras.Models
 {
     public class ProductoVM
     {
+        public int IdProducto { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string RutaImagen { get; set; }
+        public string NombreImagen { get; set; }
+        public bool Activo { get; set; } = true;
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
     }
 }
