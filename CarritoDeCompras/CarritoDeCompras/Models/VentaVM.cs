042277bb2643d9f55631ece3b63dcf309a02f7d2
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 22:55:30 -0500

    VentaVM Ok

--- CarritoDeCompras/CarritoDeCompras/Models/VentaVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/VentaVM.cs
@@ -2,5 +2,15 @@
 {
     public class VentaVM
     {
+        public int IdVenta { get; set; }
+        public int IdCliente { get; set; }
+        public int TotalProducto { get; set; }
+        public decimal MontoTotal { get; set; }
+        public string Contacto { get; set; }
+        public int IdDistrito { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public int IdTransaccion { get; set; }
     }
 }
