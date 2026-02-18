39ffcc7c440053fcd11ab5cb7c948dee5918c497
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 23:03:55 -0500

    UsuarioVM Ok

--- CarritoDeCompras/CarritoDeCompras/Models/UsuarioVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/UsuarioVM.cs
@@ -2,5 +2,13 @@
 {
     public class UsuarioVM
     {
+        public int IdUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string Reestablecer { get; set; }
+        public bool Activo { get; set; } = true;
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
     }
 }
