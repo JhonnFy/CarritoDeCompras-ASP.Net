43264b0b7b6a8479bac8029075a07edf258a90f0
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 22:48:51 -0500

    ClienteVM Ok

--- CarritoDeCompras/CarritoDeCompras/Models/ClienteVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/ClienteVM.cs
@@ -1,6 +1,21 @@
-﻿namespace CarritoDeCompras.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarritoDeCompras.Models
 {
     public class ClienteVM
     {
+        public int IdCliente { get; set; }
+        [Required]
+        public string Nombres { get; set; }
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public string Correo { get; set; }
+        [Required]
+        public string Clave { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public string Reestablecer { get; set; }
+
     }
 }
